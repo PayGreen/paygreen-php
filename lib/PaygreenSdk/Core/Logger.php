62c7c32120fbccb15c0ccc0735d82ddc136b6f71
Maxime <maxime.lemolt@outlook.com>
2021-10-28 17:04:11 +0200

    Add Logger

---
+++ lib/PaygreenSdk/Core/Logger.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Paygreen\Sdk\Core;
+
+use Exception;
+use Monolog\Formatter\LineFormatter;
+use Monolog\Logger as MonologLogger;
+use Monolog\Handler\StreamHandler;
+
+abstract class Logger
+{
+    const DATE_FORMAT = "Y-m-d H:i:s";
+    const LOG_FORMAT = "[%datetime%] | %level_name% | %message% | Data: \n %context%\n";
+
+    /**
+     * @param string $message
+     * @param array $data
+     * @throws Exception
+     */
+    static public function debug($message, $data = array())
+    {
+        self::log('debug', $message, $data);
+    }
+
+    /**
+     * @param string $message
+     * @param array $data
+     * @throws Exception
+     */
+    static public function info($message, $data = array())
+    {
+        self::log('info', $message, $data);
+    }
+
+    /**
+     * @param string $message
+     * @param array $data
+     * @throws Exception
+     */
+    static public function notice($message, $data = array())
+    {
+        self::log('notice', $message, $data);
+    }
+
+    /**
+     * @param string $message
+     * @param array $data
+     * @throws Exception
+     */
+    static public function warning($message, $data = array())
+    {
+        self::log('warning', $message, $data);
+    }
+
+    /**
+     * @param string $message
+     * @param array $data
+     * @throws Exception
+     */
+    static public function error($message, $data = array())
+    {
+        self::log('error', $message, $data);
+    }
+
+    /**
+     * @param string $message
+     * @param array $data
+     * @throws Exception
+     */
+    static public function critical($message, $data = array())
+    {
+        self::log('critical', $message, $data);
+    }
+
+    /**
+     * @param string $level
+     * @param string $message
+     * @param array $data
+     * @throws Exception
+     */
+    static private function log($level, $message, $data)
+    {
+        $logger = new MonologLogger('sdk');
+        $formatter = new LineFormatter(self::LOG_FORMAT, self::DATE_FORMAT);
+        $path = $_SERVER['DOCUMENT_ROOT'] . '/logs/sdk.log';
+
+        if (!file_exists($path)) {
+            self::createLogFile($path);
+        }
+
+        $handler = new StreamHandler($path, MonologLogger::DEBUG);
+        $handler->setFormatter($formatter);
+        $logger->pushHandler($handler);
+
+        $logger->$level($message, $data);
+    }
+
+    /**
+     * @param string $path
+     * @return void
+     */
+    static private function createLogFile($path)
+    {
+        $logFolderPath = pathinfo($path, PATHINFO_DIRNAME);
+
+        if (!file_exists($logFolderPath)) {
+            mkdir($logFolderPath);
+        }
+
+        file_put_contents($path, '', FILE_APPEND);
+    }
+}
