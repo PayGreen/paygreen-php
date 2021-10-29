1baa02ce8cdb1d637483c84c42fa03c7bc618e50
Maxime <maxime.lemolt@outlook.com>
2021-10-29 16:26:10 +0200

    Test Config component

---
+++ tests/Core/Components/ConfigTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Paygreen\Tests\Core\Components;
+use Paygreen\Sdk\Core\Components\Bag;
+use Paygreen\Sdk\Core\Components\Config;
+use PHPUnit\Framework\TestCase;
+
+final class ConfigTest extends TestCase
+{
+    public function testBagFormat()
+    {
+        $config = new Config();
+
+        $this->assertInstanceOf(Bag::class, $config->getBag());
+    }
+
+    public function testCanGetConfig()
+    {
+        $config = new Config();
+        $config['configKey'] = 'configValue';
+
+        $this->assertEquals('configValue', $config['configKey']);
+    }
+}
