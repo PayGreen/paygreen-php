c95bc83b368e9a7d9225ae75ccffc39a1000e512
Maxime <maxime.lemolt@outlook.com>
2021-11-02 18:08:57 +0100

    Handle payment api version in ApiClient

--- lib/PaygreenSdk/Payments/ApiClient.php
+++ lib/PaygreenSdk/Payments/ApiClient.php
@@ -4,10 +4,13 @@ namespace Paygreen\Sdk\Payments;
 
 use Exception;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Psr7\Response;
+use Paygreen\Sdk\Core\Components\Config;
 use Paygreen\Sdk\Core\Components\Environment;
 use Paygreen\Sdk\Core\HttpClient;
 use Paygreen\Sdk\Core\Logger;
+use Paygreen\Sdk\Payments\Exceptions\InvalidApiVersion;
 use Paygreen\Sdk\Payments\Exceptions\PaymentCreationException;
 use Paygreen\Sdk\Payments\Interfaces\OrderInterface;
 use Psr\Log\LoggerInterface;
@@ -20,6 +23,11 @@ class ApiClient extends HttpClient
     /** @var LoggerInterface */
     private $logger;
 
+    private $config = [];
+
+    /**
+     * @throws InvalidApiVersion
+     */
     public function __construct(LoggerInterface $logger = null)
     {
         if ($logger === null) {
@@ -34,6 +42,7 @@ class ApiClient extends HttpClient
 
         parent::__construct($environment);
 
+        $this->loadConfig();
         $this->initClient();
     }
 
@@ -49,7 +58,7 @@ class ApiClient extends HttpClient
      * @param string $ttl
      * @return Response
      * @throws PaymentCreationException
-     * @throws Exception
+     * @throws Exception|GuzzleException
      */
     public function createCash(
         OrderInterface $order,
@@ -64,51 +73,54 @@ class ApiClient extends HttpClient
     ) {
         $this->logger->info("Create '$paymentType' cash payment with an amount of '$amount'.");
 
+        $requestConfig = $this->config['requests.create_cash'];
+
         $url = $this->parseUrlParameters(
-            $this->getBaseUri() . '/api/{ui}/payins/transaction/cash',
+            $this->getBaseUri() . $requestConfig['url'],
             [
                 'ui' => $this->environment->getPublicKey()
             ]
         );
 
-        $response = $this->client->post($url, [
-            'json' => [
-                'orderId' => 'PG-' . $order->getReference(),
-                'amount' => $amount,
-                'currency' => $currency,
-                'paymentType' => $paymentType,
-                'notifiedUrl' => $notifiedUrl,
-                'returnedUrl' => $returnedUrl,
-                'buyer' => (object) [
-                    'id' => $order->getCustomer()->getId(),
-                    'lastName' => $order->getCustomer()->getLastName(),
-                    'firstName' => $order->getCustomer()->getFirstName(),
-                    'country' => $order->getCustomer()->getCountryCode()
-                ],
-                'shippingAddress' => (object) [
-                    'lastName' => $order->getShippingAddress()->getLastName(),
-                    'firstName' => $order->getShippingAddress()->getFirstName(),
-                    'address' => $order->getShippingAddress()->getStreet(),
-                    'zipCode' => $order->getShippingAddress()->getZipCode(),
-                    'city' => $order->getShippingAddress()->getCity(),
-                    'country' => $order->getShippingAddress()->getCountryCode()
-                ],
-                'billingAddress' => (object) [
-                    'lastName' => $order->getBillingAddress()->getLastName(),
-                    'firstName' => $order->getBillingAddress()->getFirstName(),
-                    'address' => $order->getBillingAddress()->getStreet(),
-                    'zipCode' => $order->getBillingAddress()->getZipCode(),
-                    'city' => $order->getBillingAddress()->getCity(),
-                    'country' => $order->getBillingAddress()->getCountryCode()
-                ],
-                'metadata' => $metadata,
-                'eligibleAmount' => $eligibleAmount,
-                'ttl' => $ttl
-            ],
-            'headers' => [
-                'Authorization' => 'Bearer ' . $this->environment->getPrivateKey()
-            ]
-        ]);
+        $response = $this->client->request(
+            $requestConfig['method'],
+            $url,
+            $this->buildOptions([
+                'json' => [
+                    'orderId' => 'PG-' . $order->getReference(),
+                    'amount' => $amount,
+                    'currency' => $currency,
+                    'paymentType' => $paymentType,
+                    'notifiedUrl' => $notifiedUrl,
+                    'returnedUrl' => $returnedUrl,
+                    'buyer' => (object) [
+                        'id' => $order->getCustomer()->getId(),
+                        'lastName' => $order->getCustomer()->getLastName(),
+                        'firstName' => $order->getCustomer()->getFirstName(),
+                        'country' => $order->getCustomer()->getCountryCode()
+                    ],
+                    'shippingAddress' => (object) [
+                        'lastName' => $order->getShippingAddress()->getLastName(),
+                        'firstName' => $order->getShippingAddress()->getFirstName(),
+                        'address' => $order->getShippingAddress()->getStreet(),
+                        'zipCode' => $order->getShippingAddress()->getZipCode(),
+                        'city' => $order->getShippingAddress()->getCity(),
+                        'country' => $order->getShippingAddress()->getCountryCode()
+                    ],
+                    'billingAddress' => (object) [
+                        'lastName' => $order->getBillingAddress()->getLastName(),
+                        'firstName' => $order->getBillingAddress()->getFirstName(),
+                        'address' => $order->getBillingAddress()->getStreet(),
+                        'zipCode' => $order->getBillingAddress()->getZipCode(),
+                        'city' => $order->getBillingAddress()->getCity(),
+                        'country' => $order->getBillingAddress()->getCountryCode()
+                    ],
+                    'metadata' => $metadata,
+                    'eligibleAmount' => $eligibleAmount,
+                    'ttl' => $ttl
+                ]
+            ], $requestConfig['private'])
+        );
 
         if ($response->getStatusCode() !== 200) {
             throw new PaymentCreationException(
@@ -147,5 +159,41 @@ class ApiClient extends HttpClient
                 ]
             ]
         ]);
+    }
+
+    /**
+     * @throws InvalidApiVersion
+     */
+    private function loadConfig()
+    {
+        $config = new Config();
+        $apiVersion = (int) getenv('PG_PAYMENT_API_VERSION');
+
+        switch ($apiVersion) {
+            case 2:
+                $this->config = $config['payment.v2'];
+                break;
+            case 3:
+                $this->config = $config['payment.v3'];
+                break;
+            default:
+                throw new InvalidApiVersion('Invalid API version. Value should be 2 or 3.');
+        }
+    }
+
+    /**
+     * @param array $options
+     * @param bool $private
+     * @return array
+     */
+    private function buildOptions(array $options = [], $private = true)
+    {
+        if ($private) {
+            $options['headers'] = [
+                'Authorization' => 'Bearer ' . $this->environment->getPrivateKey()
+            ];
+        }
+
+        return $options;
     }
 }
