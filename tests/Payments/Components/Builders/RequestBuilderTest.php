74980bb221dcd83260ee42d884cde01776466c90
Maxime <maxime.lemolt@outlook.com>
2021-11-05 12:09:31 +0100

    Fix tests

--- tests/Payments/Components/Builders/RequestBuilderTest.php
+++ tests/Payments/Components/Builders/RequestBuilderTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Paygreen\Sdk\Core\Components\Environment;
 use Paygreen\Sdk\Payments\Components\Builders\RequestBuilder;
 use Paygreen\Sdk\Payments\Exceptions\InvalidApiVersion;
 use PHPUnit\Framework\TestCase;
@@ -8,16 +9,30 @@ final class RequestBuilderTest extends TestCase
 {
     public function testCanBeCreatedFromValidParameters()
     {
+        $environment = new Environment(
+            'public_key',
+            'private_key',
+            'sandbox',
+            2
+        );
+
         $this->assertInstanceOf(
             RequestBuilder::class,
-            (new RequestBuilder('2', 'private_key', 'https://sandbox.paygreen.fr'))
+            (new RequestBuilder($environment))
         );
     }
 
     public function testCannotBeCreatedFromInvalidParameters()
     {
+        $environment = new Environment(
+            'public_key',
+            'private_key',
+            'sandbox',
+            'INVALID_API_VERSION'
+        );
+
         $this->expectException(InvalidApiVersion::class);
 
-        new RequestBuilder('INVALID_API_KEY', 'private_key', 'https://sandbox.paygreen.fr');
+        new RequestBuilder($environment);
     }
 }
