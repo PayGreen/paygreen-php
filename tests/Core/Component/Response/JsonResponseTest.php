4dd2906cbb7e0b987e2f188d329c421c84d9ab5f
Maxime <maxime.lemolt@outlook.com>
2021-11-05 17:17:12 +0100

    Add tests

---
+++ tests/Core/Component/Response/JsonResponseTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Paygreen\Tests\Core\Component\Response;
+
+use PHPUnit\Framework\TestCase;
+
+final class JsonResponseTest extends TestCase
+{
+    // TODO: Add tests
+}
---
+++ tests/Payment/Component/Response/ResponseTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Paygreen\Tests\Payment\Component\Response;
+
+use PHPUnit\Framework\TestCase;
+
+final class ResponseTest extends TestCase
+{
+    // TODO: Add tests
+}
