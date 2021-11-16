58e71302be820077cc8a0b19b15b1e0166a509c8
Maxime lemolt <maxime.lemolt@paygreen.fr>
2021-11-16 13:18:20 +0000

    feature/tests-models

--- lib/PaygreenSdk/Payment/Model/AddressInterface.php
+++ lib/PaygreenSdk/Payment/Model/AddressInterface.php
@@ -10,14 +10,21 @@ interface AddressInterface
     public function getStreetLineOne();
 
     /**
+     * @param string $streetLineOne
+     * @return void
+     */
+    public function setStreetLineOne($streetLineOne);
+
+    /**
      * @return string
      */
     public function getStreetLineTwo();
 
     /**
-     * @return string
+     * @param string $streetLineTwo
+     * @return void
      */
-    public function getStreet();
+    public function setStreetLineTwo($streetLineTwo);
 
     /**
      * @return string
@@ -25,9 +32,21 @@ interface AddressInterface
     public function getFirstName();
 
     /**
+     * @param string $firstname
+     * @return void
+     */
+    public function setFirstname($firstname);
+
+    /**
      * @return string
      */
     public function getLastName();
+
+    /**
+     * @param string $lastname
+     * @return void
+     */
+    public function setLastname($lastname);
 
     /**
      * @return string
@@ -35,12 +54,35 @@ interface AddressInterface
     public function getPostcode();
 
     /**
+     * @param string $postcode
+     * @return void
+     */
+    public function setPostcode($postcode);
+
+    /**
      * @return string
      */
     public function getCity();
 
     /**
+     * @param string $city
+     * @return void
+     */
+    public function setCity($city);
+
+    /**
      * @return string
      */
     public function getCountryCode();
+
+    /**
+     * @param string $countryCode
+     * @return void
+     */
+    public function setCountryCode($countryCode);
+
+    /**
+     * @return string
+     */
+    public function getStreet();
 }
---
+++ tests/Unit/Payment/Model/AddressTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Paygreen\Tests\Unit\Payment\Model;
+
+use Paygreen\Sdk\Payment\Model\Address;
+use PHPUnit\Framework\TestCase;
+
+final class AddressTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function testCanGetAndSetPostCode()
+    {
+        $address = new Address();
+        $address->setPostcode('76000');
+
+        $this->assertEquals('76000', $address->getPostcode());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCanGetAndSetCity()
+    {
+        $address = new Address();
+        $address->setCity('Rouen');
+
+        $this->assertEquals('Rouen', $address->getCity());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCanGetAndSetFirstname()
+    {
+        $address = new Address();
+        $address->setFirstname('Laurent');
+
+        $this->assertEquals('Laurent', $address->getFirstname());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCanGetAndSetLastname()
+    {
+        $address = new Address();
+        $address->setLastname('Barre');
+
+        $this->assertEquals('Barre', $address->getLastname());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCanGetAndSetCountryCode()
+    {
+        $address = new Address();
+        $address->setCountryCode('FR');
+
+        $this->assertEquals('FR', $address->getCountryCode());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCanGetAndSetStreetLineOne()
+    {
+        $address = new Address();
+        $address->setStreetLineOne('Ma rue');
+
+        $this->assertEquals('Ma rue', $address->getStreetLineOne());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCanGetAndSetStreetLineTwo()
+    {
+        $address = new Address();
+        $address->setStreetLineTwo('Escalier B');
+
+        $this->assertEquals('Escalier B', $address->getStreetLineTwo());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCanGetStreet()
+    {
+        $address = new Address();
+        $address->setStreetLineOne('Ma rue');
+
+        $this->assertEquals('Ma rue', $address->getStreet());
+
+        $address->setStreetLineTwo('Escalier B');
+
+        $this->assertEquals('Ma rue Escalier B', $address->getStreet());
+    }
+}
