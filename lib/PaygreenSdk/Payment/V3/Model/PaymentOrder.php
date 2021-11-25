4e01331d9cb9a68bb6188316f166cbe829d69a69
Romain Da Costa <romain.dacosta@paygreen.fr>
2021-11-25 11:10:10 +0000

    SPH-68 - Ajouter les champs manquants pour le PaymentOrder

--- lib/PaygreenSdk/Payment/V3/Model/PaymentOrder.php
+++ lib/PaygreenSdk/Payment/V3/Model/PaymentOrder.php
@@ -38,6 +38,81 @@ class PaymentOrder implements PaymentOrderInterface
     private $platforms;
 
     /**
+     * @var int
+     */
+    private $platformsShopId;
+
+    /**
+     * @var string
+     */
+    private $cancelUrl;
+
+    /**
+     * @var string
+     */
+    private $cycle;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @var int
+     */
+    private $eligibleAmount;
+
+    /**
+     * @var int
+     */
+    private $firstAmount;
+
+    /**
+     * @var int
+     */
+    private $instrumentId;
+
+    /**
+     * @var int
+     */
+    private $instrumentTTL;
+
+    /**
+     * @var bool
+     */
+    private $merchantInitiated;
+
+    /**
+     * @var string
+     */
+    private $notificationUrl;
+
+    /**
+     * @var int
+     */
+    private $occurences;
+
+    /**
+     * @var int
+     */
+    private $previousOrderId;
+
+    /**
+     * @var string
+     */
+    private $paymentDay;
+
+    /**
+     * @var string
+     */
+    private $returnUrl;
+
+    /**
+     * @var string
+     */
+    private $startAt;
+
+    /**
      * @return array
      */
     public function getPlatforms()
@@ -131,5 +206,245 @@ class PaymentOrder implements PaymentOrderInterface
     public function setPartialAllowed($partialAllowed)
     {
         $this->partialAllowed = $partialAllowed;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCancelUrl()
+    {
+        return $this->cancelUrl;
+    }
+
+    /**
+     * @param string $cancelUrl
+     */
+    public function setCancelUrl($cancelUrl)
+    {
+        $this->cancelUrl = $cancelUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCycle()
+    {
+        return $this->cycle;
+    }
+
+    /**
+     * @param string $cycle
+     */
+    public function setCycle($cycle)
+    {
+        $this->cycle = $cycle;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return int
+     */
+    public function getEligibleAmount()
+    {
+        return $this->eligibleAmount;
+    }
+
+    /**
+     * @param int $eligibleAmount
+     */
+    public function setEligibleAmount($eligibleAmount)
+    {
+        $this->eligibleAmount = $eligibleAmount;
+    }
+
+    /**
+     * @return int
+     */
+    public function getFirstAmount()
+    {
+        return $this->firstAmount;
+    }
+
+    /**
+     * @param int $firstAmount
+     */
+    public function setFirstAmount($firstAmount)
+    {
+        $this->firstAmount = $firstAmount;
+    }
+
+    /**
+     * @return int
+     */
+    public function getInstrumentId()
+    {
+        return $this->instrumentId;
+    }
+
+    /**
+     * @param int $instrumentId
+     */
+    public function setInstrumentId($instrumentId)
+    {
+        $this->instrumentId = $instrumentId;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isMerchantInitiated()
+    {
+        return $this->merchantInitiated;
+    }
+
+    /**
+     * @param bool $merchantInitiated
+     */
+    public function setMerchantInitiated($merchantInitiated)
+    {
+        $this->merchantInitiated = $merchantInitiated;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNotificationUrl()
+    {
+        return $this->notificationUrl;
+    }
+
+    /**
+     * @param string $notificationUrl
+     */
+    public function setNotificationUrl($notificationUrl)
+    {
+        $this->notificationUrl = $notificationUrl;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOccurences()
+    {
+        return $this->occurences;
+    }
+
+    /**
+     * @param int $occurences
+     */
+    public function setOccurences($occurences)
+    {
+        $this->occurences = $occurences;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPreviousOrderId()
+    {
+        return $this->previousOrderId;
+    }
+
+    /**
+     * @param int $previousOrderId
+     */
+    public function setPreviousOrderId($previousOrderId)
+    {
+        $this->previousOrderId = $previousOrderId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPlatformsShopId()
+    {
+        return $this->platformsShopId;
+    }
+
+    /**
+     * @param int $platformsShopId
+     */
+    public function setPlatformsShopId($platformsShopId)
+    {
+        $this->platformsShopId = $platformsShopId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPaymentDay()
+    {
+        return $this->paymentDay;
+    }
+
+    /**
+     * @param string $paymentDay
+     */
+    public function setPaymentDay($paymentDay)
+    {
+        $this->paymentDay = $paymentDay;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReturnUrl()
+    {
+        return $this->returnUrl;
+    }
+
+    /**
+     * @param string $returnUrl
+     */
+    public function setReturnUrl($returnUrl)
+    {
+        $this->returnUrl = $returnUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStartAt()
+    {
+        return $this->startAt;
+    }
+
+    /**
+     * @param string $startAt
+     */
+    public function setStartAt($startAt)
+    {
+        $this->startAt = $startAt;
+    }
+
+    /**
+     * @return int
+     */
+    public function getInstrumentTTL()
+    {
+        return $this->instrumentTTL;
+    }
+
+    /**
+     * @param int $instrumentTTL
+     */
+    public function setInstrumentTTL($instrumentTTL)
+    {
+        $this->instrumentTTL = $instrumentTTL;
     }
 }
--- lib/PaygreenSdk/Payment/V3/Model/PaymentOrderInterface.php
+++ lib/PaygreenSdk/Payment/V3/Model/PaymentOrderInterface.php
@@ -63,4 +63,154 @@ interface PaymentOrderInterface
      * @param bool $partialAllowed
      */
     public function setPartialAllowed($partialAllowed);
+
+    /**
+     * @return string
+     */
+    public function getCancelUrl();
+
+    /**
+     * @param string $cancelUrl
+     */
+    public function setCancelUrl($cancelUrl);
+
+    /**
+     * @return string
+     */
+    public function getCycle();
+
+    /**
+     * @param string $cycle
+     */
+    public function setCycle($cycle);
+
+    /**
+     * @return string
+     */
+    public function getDescription();
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description);
+
+    /**
+     * @return int
+     */
+    public function getEligibleAmount();
+
+    /**
+     * @param int $eligibleAmount
+     */
+    public function setEligibleAmount($eligibleAmount);
+
+    /**
+     * @return int
+     */
+    public function getFirstAmount();
+
+    /**
+     * @param int $firstAmount
+     */
+    public function setFirstAmount($firstAmount);
+
+    /**
+     * @return int
+     */
+    public function getInstrumentId();
+
+    /**
+     * @param int $instrumentId
+     */
+    public function setInstrumentId($instrumentId);
+
+    /**
+     * @return bool
+     */
+    public function isMerchantInitiated();
+
+    /**
+     * @param bool $merchantInitiated
+     */
+    public function setMerchantInitiated($merchantInitiated);
+
+    /**
+     * @return string
+     */
+    public function getNotificationUrl();
+
+    /**
+     * @param string $notificationUrl
+     */
+    public function setNotificationUrl($notificationUrl);
+
+    /**
+     * @return int
+     */
+    public function getOccurences();
+
+    /**
+     * @param int $occurences
+     */
+    public function setOccurences($occurences);
+
+    /**
+     * @return int
+     */
+    public function getPreviousOrderId();
+
+    /**
+     * @param int $previousOrderId
+     */
+    public function setPreviousOrderId($previousOrderId);
+
+    /**
+     * @return int
+     */
+    public function getPlatformsShopId();
+
+    /**
+     * @param int $platformsShopId
+     */
+    public function setPlatformsShopId($platformsShopId);
+
+    /**
+     * @return string
+     */
+    public function getPaymentDay();
+
+    /**
+     * @param string $paymentDay
+     */
+    public function setPaymentDay($paymentDay);
+
+    /**
+     * @return string
+     */
+    public function getReturnUrl();
+
+    /**
+     * @param string $returnUrl
+     */
+    public function setReturnUrl($returnUrl);
+
+    /**
+     * @return string
+     */
+    public function getStartAt();
+
+    /**
+     * @param string $startAt
+     */
+    public function setStartAt($startAt);
+
+    /**
+     * @return int
+     */
+    public function getInstrumentTTL();
+
+    /**
+     * @param int $instrumentTTL
+     */
+    public function setInstrumentTTL($instrumentTTL);
 }
