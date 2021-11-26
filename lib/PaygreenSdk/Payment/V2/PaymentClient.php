344bd60f39ee3d6f4c67473a3d31e8e25d3fc313
Maxime lemolt <maxime.lemolt@paygreen.fr>
2021-11-26 08:40:04 +0000

    feature/oauth requests

--- lib/PaygreenSdk/Payment/V2/PaymentClient.php
+++ lib/PaygreenSdk/Payment/V2/PaymentClient.php
@@ -4,9 +4,11 @@ namespace Paygreen\Sdk\Payment\V2;
 
 use Exception;
 use Http\Client\Exception as HttpClientException;
+use Paygreen\Sdk\Core\Exception\ConstraintViolationException;
 use Paygreen\Sdk\Core\Response\JsonResponse;
 use Paygreen\Sdk\Payment\Client;
 use Paygreen\Sdk\Payment\V2\Model\PaymentOrder;
+use Paygreen\Sdk\Payment\V2\Request\OAuthRequest;
 use Paygreen\Sdk\Payment\V2\Request\PaymentOrder\CancelRequest;
 use Paygreen\Sdk\Payment\V2\Request\PaymentOrder\CashRequest;
 use Paygreen\Sdk\Payment\V2\Request\PaymentOrder\RecurringRequest;
@@ -18,6 +20,102 @@ use Paygreen\Sdk\Payment\V2\Request\PaymentTypeRequest;
 
 class PaymentClient extends Client
 {
+    /**
+     * @param string      $ipAddress
+     * @param string      $email
+     * @param string      $name
+     * @param null|string $phoneNumber
+     * @param null|string $serverAddress
+     *
+     * @throws HttpClientException
+     * @throws Exception
+     * @throws ConstraintViolationException
+     *
+     * @return JsonResponse
+     */
+    public function createOAuthAccessToken(
+        $ipAddress,
+        $email,
+        $name,
+        $phoneNumber = null,
+        $serverAddress = null
+    ) {
+        $this->logger->info('Create OAuth access token.');
+
+        $request = (new OAuthRequest($this->requestFactory, $this->environment))->getCreateTokenRequest(
+            $ipAddress,
+            $email,
+            $name,
+            $phoneNumber,
+            $serverAddress
+        );
+
+        $this->setLastRequest($request);
+
+        $response = $this->sendRequest($request);
+        $this->setLastResponse($response);
+
+        if (200 === $response->getStatusCode()) {
+            $this->logger->info('OAuth access token successfully created.');
+        }
+
+        return new JsonResponse($response);
+    }
+
+    /**
+     * @param string $clientId
+     * @param string $redirectUri
+     * @param string $responseType
+     *
+     * @throws ConstraintViolationException
+     * @throws Exception
+     *
+     * @return string
+     */
+    public function getOAuthAuthenticationPage($clientId, $redirectUri, $responseType = 'code')
+    {
+        $this->logger->info('Get OAuth authentication page.');
+
+        return (new OAuthRequest($this->requestFactory, $this->environment))->getAuthenticationPageRequest(
+            $clientId,
+            $redirectUri,
+            $responseType
+        );
+    }
+
+    /**
+     * @param string $clientId
+     * @param string $code
+     * @param string $grantType
+     *
+     * @throws ConstraintViolationException
+     * @throws HttpClientException
+     * @throws Exception
+     *
+     * @return JsonResponse
+     */
+    public function controlOAuthAuthentication($clientId, $code, $grantType = 'authorization_code')
+    {
+        $this->logger->info('Control OAuth authentication.');
+
+        $request = (new OAuthRequest($this->requestFactory, $this->environment))->getAuthenticationControlRequest(
+            $clientId,
+            $grantType,
+            $code
+        );
+
+        $this->setLastRequest($request);
+
+        $response = $this->sendRequest($request);
+        $this->setLastResponse($response);
+
+        if (200 === $response->getStatusCode()) {
+            $this->logger->info('OAuth authentication is valid.');
+        }
+
+        return new JsonResponse($response);
+    }
+
     /**
      * @param PaymentOrder $paymentOrder
      *
@@ -250,7 +348,7 @@ class PaymentClient extends Client
 
     /**
      * @param string $transactionId
-     * @param int $amount
+     * @param int    $amount
      *
      * @throws HttpClientException
      * @throws Exception
