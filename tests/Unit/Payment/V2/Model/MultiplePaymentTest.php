0bc136781e6247ca210bfd6bd5b993ab56e52823
Maxime <maxime.lemolt@outlook.com>
2021-11-30 13:54:43 +0100

    Fix unit tests

--- tests/Unit/Payment/V2/Model/MultiplePaymentTest.php
+++ tests/Unit/Payment/V2/Model/MultiplePaymentTest.php
@@ -2,61 +2,50 @@
 
 namespace Paygreen\Tests\Unit\Payment\V2\Model;
 
-use Paygreen\Sdk\Payment\V2\Model\OrderDetails;
+use Paygreen\Sdk\Payment\V2\Model\MultiplePayment;
 use PHPUnit\Framework\TestCase;
 
-final class OrderDetailsTest extends TestCase
+/**
+ * @internal
+ * @coversNothing
+ */
+final class MultiplePaymentTest extends TestCase
 {
-    /**
-     * @return void
-     */
     public function testCanGetAndSetCycle()
     {
-        $orderDetails = new OrderDetails();
+        $orderDetails = new MultiplePayment();
         $orderDetails->setCycle(40);
 
         $this->assertEquals(40, $orderDetails->getCycle());
     }
 
-    /**
-     * @return void
-     */
     public function testCanGetAndSetCount()
     {
-        $orderDetails = new OrderDetails();
+        $orderDetails = new MultiplePayment();
         $orderDetails->setCount(12);
 
         $this->assertEquals(12, $orderDetails->getCount());
     }
 
-    /**
-     * @return void
-     */
     public function testCanGetAndSetDay()
     {
-        $orderDetails = new OrderDetails();
+        $orderDetails = new MultiplePayment();
         $orderDetails->setDay(20);
 
         $this->assertEquals(20, $orderDetails->getDay());
     }
 
-    /**
-     * @return void
-     */
     public function testCanGetAndSetStartAt()
     {
-        $orderDetails = new OrderDetails();
+        $orderDetails = new MultiplePayment();
         $orderDetails->setStartAt(1637227163);
 
         $this->assertEquals(1637227163, $orderDetails->getStartAt());
     }
 
-    /**
-     * @return void
-     */
     public function testCanGetAndSetFirstAmount()
     {
-        $orderDetails = new OrderDetails();
+        $orderDetails = new MultiplePayment();
         $orderDetails->setFirstAmount(6500);
 
         $this->assertEquals(6500, $orderDetails->getFirstAmount());
