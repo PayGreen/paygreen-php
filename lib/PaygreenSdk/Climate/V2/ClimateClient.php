4a30f031e927e4fa017ab8421d2555e7fc454f31
Maxime <maxime.lemolt@outlook.com>
2021-12-07 12:24:08 +0100

    Implement login requests

--- lib/PaygreenSdk/Climate/V2/ClimateClient.php
+++ lib/PaygreenSdk/Climate/V2/ClimateClient.php
@@ -2,8 +2,78 @@
 
 namespace Paygreen\Sdk\Climate\V2;
 
+use Exception;
+use Paygreen\Sdk\Climate\V2\Request\LoginRequest;
+use Paygreen\Sdk\Core\Exception\ConstraintViolationException;
 use Paygreen\Sdk\Core\GreenClient;
+use Psr\Http\Message\ResponseInterface;
 
 class ClimateClient extends GreenClient
 {
+    /**
+     * @param string      $clientId
+     * @param string      $username
+     * @param string      $password
+     *
+     * @throws ConstraintViolationException
+     * @throws Exception
+     *
+     * @return ResponseInterface
+     */
+    public function login(
+        $clientId,
+        $username,
+        $password
+    ) {
+        $this->logger->info("Login to climate API with client id '{$clientId}' and username '{$username}'.");
+
+        $request = (new LoginRequest($this->requestFactory, $this->environment))->getFirstAuthenticationRequest(
+            $clientId,
+            $username,
+            $password
+        );
+
+        $this->setLastRequest($request);
+
+        $response = $this->sendRequest($request);
+        $this->setLastResponse($response);
+
+        if (200 === $response->getStatusCode()) {
+            $this->logger->info('Successful connection to the API.');
+        }
+
+        return $response;
+    }
+
+    /**
+     * @param string      $clientId
+     * @param string      $refreshToken
+     *
+     * @throws ConstraintViolationException
+     * @throws Exception
+     *
+     * @return ResponseInterface
+     */
+    public function refresh(
+        $clientId,
+        $refreshToken
+    ) {
+        $this->logger->info("Refresh connection with token : '{$refreshToken}'.");
+
+        $request = (new LoginRequest($this->requestFactory, $this->environment))->getRefreshTokenRequest(
+            $clientId,
+            $refreshToken
+        );
+
+        $this->setLastRequest($request);
+
+        $response = $this->sendRequest($request);
+        $this->setLastResponse($response);
+
+        if (200 === $response->getStatusCode()) {
+            $this->logger->info('Climate tokens successfully refreshed.');
+        }
+
+        return $response;
+    }
 }
