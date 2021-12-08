021251b502a1b08b7dc42b991bbc6bd9943d843f
Maxime lemolt <maxime.lemolt@paygreen.fr>
2021-12-08 10:51:29 +0000

    feature/climate login route

---
+++ tests/Application/climate_v2.php
@@ -0,0 +1,27 @@
+<?php
+
+use Http\Client\Curl\Client;
+use Paygreen\Sdk\Climate\V2\ClimateClient;
+use Paygreen\Sdk\Core\GreenEnvironment;
+
+$curl = new Client();
+
+$environment = new GreenEnvironment(
+    getenv('PG_CLIMATE_CLIENT_ID'),
+    getenv('PG_CLIMATE_API_SERVER'),
+    getenv('PG_CLIMATE_API_VERSION')
+);
+
+$client = new ClimateClient($curl, $environment);
+
+$response = $client->login('moduleTree', 'moduleTree', 'moduleTree');
+$responseData = json_decode($response->getBody()->getContents());
+dump($responseData);
+
+$client->setBearer($responseData->access_token);
+
+$response = $client->refresh('moduleTree', $responseData->refresh_token);
+$responseData = json_decode($response->getBody()->getContents());
+dump($responseData);
+
+$client->setBearer($responseData->access_token);
