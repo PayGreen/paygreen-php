02b91dba715e0319f36c2f7d9fc164bdb38cbbf4
Romain Da Costa <romain.dacosta@paygreen.fr>
2021-12-22 16:02:18 +0000

    SPH-104 - Charity : implémenter la route login

---
+++ tests/Application/charity_v2.php
@@ -0,0 +1,35 @@
+<?php
+
+use Http\Client\Curl\Client;
+
+$curl = new Client();
+
+$environment = new \Paygreen\Sdk\Charity\V2\Environment(
+    getenv('PG_CHARITY_CLIENT_ID'),
+    getenv('PG_CHARITY_API_SERVER'),
+    getenv('PG_CHARITY_API_VERSION')
+);
+
+$environment->setTestMode(true);
+
+$client = new \Paygreen\Sdk\Charity\V2\Client($curl, $environment);
+
+$response = $client->login('poleintegration_5', 'poleintegration_5', 'poleintegration_5');
+$responseData = json_decode($response->getBody()->getContents());
+dump($responseData);
+/*
+$client->setBearer($responseData->access_token);
+
+$response = $client->refresh('moduleTree', $responseData->refresh_token);
+$responseData = json_decode($response->getBody()->getContents());
+dump($responseData);
+
+$client->setBearer($responseData->access_token);*/
+
+// $response = $client->getAccountInfos('moduleTree');
+// $responseData = json_decode($response->getBody()->getContents());
+// dump($responseData);
+// 
+// $response = $client->getUserInfos('moduleTree', 'moduleTree');
+// $responseData = json_decode($response->getBody()->getContents());
+// dump($responseData);
--- tests/Application/index.php
+++ tests/Application/index.php
@@ -9,4 +9,5 @@ require dirname(dirname(__DIR__)) . '/vendor/autoload.php';
 (new Dotenv())->load(__DIR__ . '/.env');
 
 //require __DIR__ . DIRECTORY_SEPARATOR . 'payment_v' . getenv('PG_PAYMENT_API_VERSION') . '.php';
-require __DIR__ . DIRECTORY_SEPARATOR . 'climate_v' . getenv('PG_CLIMATE_API_VERSION') . '.php';
+//require __DIR__ . DIRECTORY_SEPARATOR . 'climate_v' . getenv('PG_CLIMATE_API_VERSION') . '.php';
+require __DIR__ . DIRECTORY_SEPARATOR . 'charity_v' . getenv('PG_CHARITY_API_VERSION') . '.php';
