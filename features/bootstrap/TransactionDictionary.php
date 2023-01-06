fcbbbc451e7f5c03ba6ef9979481e9210b9ddd8c
maximelemolt <maxime.lemolt@paygreen.fr>
2023-01-06 18:05:52 +0100

    Get transaction id

--- features/bootstrap/TransactionDictionary.php
+++ features/bootstrap/TransactionDictionary.php
@@ -1,18 +1,106 @@
 <?php
 
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\WebDriverExpectedCondition;
+use Paygreen\Sdk\Payment\V3\Model\PaymentOrder;
 use PHPUnit\Framework\Assert;
 
 /**
- * Defines payment config related features.
+ * Defines transaction related features.
  */
 trait TransactionDictionary
 {
+    /**
+     * @var string
+     */
+    private $transactionId;
+
+    /**
+     * @When /^I get a transaction id$/
+     */
+    public function iGetATransactionId()
+    {
+        $this->paymentOrder->setBuyer($this->buyer);
+        $this->paymentOrder->setAutoCapture(false);
+        $response = $this->client->createPaymentOrder($this->paymentOrder);
+
+        $data = json_decode((string)$response->getBody())->data;
+        $this->paymentOrder->setId($data->id);
+        $this->objectSecret = $data->object_secret;
+
+        // Authorize with PGJS
+        $driver = RemoteWebDriver::create('http://selenium:4444', DesiredCapabilities::firefox());
+        $driver->get('http://host.docker.internal/payment_v3_create_payment.php?' .
+            http_build_query([
+                'publicKey' => getenv('PUBLIC_KEY'),
+                'paymentOrderId' => $this->paymentOrder->getId(),
+                'objectSecret' => $this->objectSecret,
+                'instrumentId' => $this->instrumentId,
+            ]));
+
+        if (null === $this->instrumentId) {
+            // Wait until iframe is loaded
+            $driver->wait()->until(
+                WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector('#paygreen-pan-frame iframe'))
+            );
+
+            sleep(3);
+
+            // Fill pan field
+            $iframe = $driver->findElement(WebDriverBy::id('cardNumberFrame'));
+            $driver->switchTo()->frame($iframe);
+            $driver->wait()->until(
+                WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector('input[name="pan"]'))
+            );
+            $driver->findElement(WebDriverBy::name('pan'))->sendKeys(getenv('BANK_CARD_PAN'));
+
+            // Fill cvv field
+            $driver->switchTo()->defaultContent();
+            $iframe = $driver->findElement(WebDriverBy::id('cvvFrame'));
+            $driver->switchTo()->frame($iframe);
+            $driver->wait()->until(
+                WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector('input[name="cvv"]'))
+            );
+            $driver->findElement(WebDriverBy::name('cvv'))->sendKeys(getenv('BANK_CARD_CVV'));
+
+            // Fill expiration field
+            $driver->switchTo()->defaultContent();
+            $iframe = $driver->findElement(WebDriverBy::id('expFrame'));
+            $driver->switchTo()->frame($iframe);
+            $driver->wait()->until(
+                WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector('input[name="exp"]'))
+            );
+            $driver->findElement(WebDriverBy::name('exp'))->sendKeys(getenv('BANK_CARD_EXP'));
+
+            // Submit form
+            $driver->switchTo()->defaultContent();
+            $driver->executeScript('paygreenjs.submitPayment()');
+        }
+
+        $driver->wait()->until(
+            function ($driver) {
+                return $driver->executeScript('return paymentDone === true');
+            }
+        );
+
+        $success = $driver->executeScript('return paymentDone');
+        $driver->quit();
+
+        Assert::assertTrue($success);
+
+        $response = $this->client->capturePaymentOrder($this->paymentOrder->getId());
+        $data = json_decode((string)$response->getBody())->data;
+        $this->transactionId = $data->transactions[0]->id;
+    }
+
     /**
      * @When /^I get the transaction$/
      */
     public function iGetTheTransaction()
     {
-        $response = $this->client->getTransaction('tr_d7e4181dfa664a4889fce1868d785bac');
+        $this->client->getTransaction($this->transactionId);
     }
 
     /**
--- features/transaction.feature
+++ features/transaction.feature
@@ -4,10 +4,13 @@ Feature: Manage transaction
   Being authenticated
 
   Background: I have an authenticated Client
+    Given A payment order object
+    Given A buyer object
     Given A ready to use Client
     And I authenticate the Client
 
   Scenario: Get the transaction
+    When I get a transaction id
     When I get the transaction
     Then I receive a 200 status code
     And I receive a response with the transaction
