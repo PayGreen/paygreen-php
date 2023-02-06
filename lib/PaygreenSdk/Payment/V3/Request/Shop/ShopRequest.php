b7e81f147eb4f74e54bfbd9d675133846c23d484
maximelemolt <maxime.lemolt@paygreen.fr>
2023-02-06 17:30:13 +0100

    refacto

--- lib/PaygreenSdk/Payment/V3/Request/Shop/ShopRequest.php
+++ lib/PaygreenSdk/Payment/V3/Request/Shop/ShopRequest.php
@@ -75,36 +75,8 @@ class ShopRequest extends \Paygreen\Sdk\Core\Request\Request
             $shop->setNationalId($nationalId);
         }
 
-        $body = [
-            'name' => $shop->getName(),
-            'national_id' => $shop->getNationalId(),
-            'mcc' => $shop->getMcc(),
-            'annual_processing_volume' => $shop->getAnnualProcessingVolume(),
-            'average_transaction_value' => $shop->getAverageTransactionValue(),
-            'highest_transaction_value' => $shop->getHighestTransactionValue(),
-            'activity_categories' => $shop->getActivityCategories(),
-            'activity_description' => $shop->getActivityDescription(),
-            'commercial_name' => $shop->getCommercialName(),
-            'creation_date' => $shop->getCreationDate() ? $shop->getCreationDate()->format('Y-m-d') : null,
-            'economic_model' => $shop->getEconomicModel(),
-            'legal_category' => $shop->getLegalCategory(),
-            'primary_activity' => $shop->getPrimaryActivity(),
-            'product_type' => $shop->getProductType(),
-            'website_url' => $shop->getWebsiteUrl(),
-            'legal_notice_url' => $shop->getLegalNoticeUrl(),
-            'address' => null
-        ];
-
-        if ($shop->getAddress() instanceof Address) {
-            $body['address'] = [
-                "line_1" => $shop->getAddress()->getStreetLineOne(),
-                "line_2" => $shop->getAddress()->getStreetLineTwo(),
-                "postal_code" => $shop->getAddress()->getPostalCode(),
-                "city" => $shop->getAddress()->getCity(),
-                "country" => $shop->getAddress()->getCountryCode(),
-                "state" => $shop->getAddress()->getState(),
-            ];
-        }
+        $body = $this->getBodyData($shop);
+        $body['creation_date'] = $shop->getCreationDate() ? $shop->getCreationDate()->format('Y-m-d') : null;
 
         return $this->requestFactory->create(
             "/account/shops",
@@ -124,6 +96,20 @@ class ShopRequest extends \Paygreen\Sdk\Core\Request\Request
             $shopId = $this->environment->getShopId();
         }
 
+        $body = $this->getBodyData($shop);
+
+        return $this->requestFactory->create(
+            "/account/shops/$shopId",
+            (new Serializer([new CleanEmptyValueNormalizer()], [new JsonEncoder()]))->serialize($body, 'json')
+        )->withAuthorization()->isJson()->getRequest();
+    }
+
+    /**
+     * @param Shop $shop
+     * @return array
+     */
+    private function getBodyData(Shop $shop)
+    {
         $body = [
             'name' => $shop->getName(),
             'national_id' => $shop->getNationalId(),
@@ -155,9 +141,6 @@ class ShopRequest extends \Paygreen\Sdk\Core\Request\Request
             ];
         }
 
-        return $this->requestFactory->create(
-            "/account/shops/$shopId",
-            (new Serializer([new CleanEmptyValueNormalizer()], [new JsonEncoder()]))->serialize($body, 'json')
-        )->withAuthorization()->isJson()->getRequest();
+        return $body;
     }
 }
